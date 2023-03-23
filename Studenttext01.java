417920be081c600427e3b8a19383b6a06281a62f
花花的世界蝴蝶多 <74061318+TheshyL@users.noreply.github.com>
2023-03-23 22:36:31 +0800

    Add files via upload

---
+++ Studenttext01.java
@@ -0,0 +1,13 @@
+public class Studenttext01 {
+    //static int no;不创建no对象内存不存在的
+    
+    //new运算符 的作用是创建对象在jvm堆内存开辟新的内存空间
+   
+    int no;
+    String name;
+    int age;
+    boolean sex;
+    String  addr;//地址
+
+
+}
---
+++ User.java
@@ -0,0 +1,9 @@
+public class User {
+    //用户编号
+    int no;
+    //用户名
+    String name;
+    //地址
+    Address addr;
+    //Address是一种 引用 数据类型代表引用家庭地址 
+}
